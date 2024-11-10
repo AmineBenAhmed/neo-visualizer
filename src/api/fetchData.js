7c7ebd9eeabe5cb027c75b400dc26986f323a2a9
aminejih <amine@armadadelivery.com>
2024-11-10 14:29:57 +0100

    ofix env variables names

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-const API_URL = import.meta.API_URL;
-const API_KEY = import.meta.API_KEY;
+const API_URL = import.meta.env.VITE_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 const fetchData = async () => {
   const response = await axios.get(`${API_URL}?api_key=${API_KEY}`)
