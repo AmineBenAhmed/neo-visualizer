1db73a5950333c9a58cede6aa9390f7775c06c08
aminejih <amine@armadadelivery.com>
2024-11-10 18:20:11 +0100

    add dropdown to filter NEOs by orbital body

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -3,26 +3,43 @@ import { useEffect } from 'react';
 import { useState } from 'react'
 import fetchData from '../api/fetchData';
 import ChartView from '../components/ChartView';
+import DropdownFilter from '../components/DropdownFilter';
 
 const Dashboard = () => {
   const [apiData, setApiData] = useState([]);
   const [loading, setLoading] = useState(true)
+  const [selectedObject, setSelectedObject] = useState('');
+
+  const orbitalBodies = Array.from(
+    new Set(
+        apiData.flatMap((item) => 
+            item.close_approach_data.map((approach) => approach.orbiting_body)
+        )
+    ));
 
   useEffect(() => {
     fetchData()
-      .then( data => {setApiData(data);})
+      .then( data => setApiData(data))
       .finally(() => setLoading(false))  
     
-    }, [])
+    }, []);
+
   
+  const filteredData = selectedObject
+  ? apiData.filter(item => 
+    item.close_approach_data.some((approach) => approach.orbiting_body === selectedObject)
+  )
+  : apiData
+
   if (loading) return <p>Loading api data...</p>
 
   return (
-    <div>
-      <h1 className='text-container text-2xl font-bold my-4 text-sm' >NASA NEO Data Visualizer!</h1>
-
-        <ChartView data={apiData} />
-   
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-center lg:text-2xl font-bold my-10 sm:text-sm" >NASA NEO Data Visualizer!</h1>
+      <div className='lg:ml-[90svw] lg:w-[40%] md:ml-[90svw] md:w-[50%]' >
+        <DropdownFilter  options={orbitalBodies} selected={selectedObject} onChange={setSelectedObject}/>
+      </div>
+      <ChartView data={filteredData} />
     </div>
   )
 }
