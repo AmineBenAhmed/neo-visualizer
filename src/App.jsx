539f3ea0f49a018b31e5a1bf4cdd42793eb9a687
aminejih <amine@armadadelivery.com>
2024-11-11 10:47:08 +0100

    add table view and button to toggle between chart and table

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Dashboard from "./views/Dashboard"
 function App() {
 
   return (
-      <div className="lg:font-bold" >
-        <Dashboard />
+      <div className="p-2 bg-auto bg-no-repeat bg-center bg-[url('./assets/bodies.jpg')] lg:overflow-hidden md:overflow-y-auto sm:overflow-y-auto h-[100vh]" >
+        <div className="m-1 overflow-hidden">
+          <Dashboard />
+        </div>
       </div>
   )
 }
--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -4,14 +4,21 @@ const API_URL = import.meta.env.VITE_API_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 const fetchData = async () => {
-  const response = await axios.get(`${API_URL}?api_key=${API_KEY}`)
-  return response?.data?.near_earth_objects?.sort((a,b) => {
+  try {
+   const response = await axios.get(`${API_URL}?api_key=${API_KEY}`)
+    
+    return response?.data?.near_earth_objects?.sort((a,b) => {
     const avrA = (a.estimated_diameter.kilometers.estimated_diameter_max + a.estimated_diameter.kilometers.estimated_diameter_max) / 2;
     const avrB = (b.estimated_diameter.kilometers.estimated_diameter_max + b.estimated_diameter.kilometers.estimated_diameter_max) / 2;
+    
     if (avrB >= avrA) return 1
     else if (avrA === avrB) return 0
     else return -1
   })
+  } catch (error) {
+    console.log(error)
+    return []
+  }
 }
 
 
